ff7d38c6b1e61006e9c75a55cd79e12bdbad5d48
Maro Kiri <marokiri8@gmail.com>
2025-07-11 00:53:50 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
+  },
+  experimental: {
+      serverActions: {},
+  },
+  allowedDevOrigins: ['*.cloudworkstations.dev'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;

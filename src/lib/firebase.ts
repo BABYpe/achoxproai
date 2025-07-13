a8b39d3bbb63d3b4cb1c4bfb47bf4baca17be034
Maro Kiri <marokiri8@gmail.com>
2025-07-13 12:24:25 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,16 +5,18 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBmo6oykjhmjq2zKRokuxFj_OAiT7kJfzQ",
+  authDomain: "achox-pro.firebaseapp.com",
+  databaseURL: "https://achox-pro-default-rtdb.firebaseio.com",
+  projectId: "achox-pro",
+  storageBucket: "achox-pro.appspot.com",
+  messagingSenderId: "25667835163",
+  appId: "1:25667835163:web:fafe4897001a75f3822fbe",
+  measurementId: "G-D57PQVJXJW"
 };
+
 
 // Initialize Firebase for client-side
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -23,13 +25,12 @@ const storage = getStorage(app);
 
 // Initialize Analytics only on the client and if supported
 let analytics;
-// Disabling Analytics initialization to prevent permission errors
-// if (typeof window !== 'undefined') {
-//   isSupported().then(yes => {
-//     if (yes) {
-//       analytics = getAnalytics(app);
-//     }
-//   });
-// }
+if (typeof window !== 'undefined') {
+  isSupported().then(yes => {
+    if (yes) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
 
 export { app, db, storage, analytics };
